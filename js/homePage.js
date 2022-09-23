068c2e408a3200cea5d98bcdd7477bd24a7906dd
Erin Shi <eshi@umich.edu>
2022-09-23 17:29:33 -0400

    under construction, taking down website

--- js/homePage.js
+++ js/homePage.js
@@ -14,29 +14,28 @@ function currentSlide(n) {
 }
 
 function showDivs(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+    // var i;
+    // var slides = document.getElementsByClassName("mySlides");
+    // if (n > slides.length) {slideIndex = 1}
+    // if (n < 1) {slideIndex = slides.length}
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = "none";
+    // }
+    // slides[slideIndex-1].style.display = "block";
 }
 
 //automatically switch slides
 function switchSlide() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-        slideIndex = 1
-    }
-    x[slideIndex-1].style.display = "block";
-    setTimeout(switchSlide, 3000);
+    // var i;
+    // var x = document.getElementsByClassName("mySlides");
+    // for (i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none";
+    // }
+    // slideIndex++;
+    // if (slideIndex > x.length) {
+    //     slideIndex = 1
+    // }
+    // x[slideIndex-1].style.display = "block";
 }
 
 // < > space keys to switch
@@ -48,3 +47,27 @@ window.addEventListener("keydown", (event) => {
         plusSlides(1);
     }
   });
+
+
+
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    console.log("yes");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
